3d873e4dab6d6c135855fe7432879d8ba76811b7
Gustavo  Coutinho <guscoutinho@outlook.com>
2020-11-11 17:00:02 -0500

    folders setup

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+// load data using promises
+// let promises = [
+//
+//     d3.csv("data/christmas_lyrics.csv"),
+//     d3.csv("data/census_usa.csv")
+// ];
+//
+// Promise.all(promises)
+//     .then( function(data){ initMainPage(data) })
+//     .catch( function (err){console.log(err)} );
